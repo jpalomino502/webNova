21f07d19d7a1762fac3b39e2578d243329c34903
jpalomino502 <joseph.palomino0102@gmail.com>
2025-01-08 22:26:12 -0500

    Agregar página de Servicios y actualizar rutas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/common/header';
 import Footer from './components/common/Footer';
 import Home from './pages/home';
+import Service from './pages/service';
 import NotFound from './pages/NotFound';
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="servicios" element={<Service />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
---
+++ src/pages/service.jsx
@@ -0,0 +1,7 @@
+export default function Servicicos () {
+    return (
+        <div>
+            <h1>Servicios</h1>
+        </div>
+    );
+}

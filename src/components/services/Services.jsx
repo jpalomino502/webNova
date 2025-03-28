33a63ed7eb52dfecda1176ae3e016061d17a6de8
jpalomino502 <jpalomino502@unab.edu.co>
2025-03-28 01:55:08 -0500

    Agregar imagen descriptiva y mejorar la estructura del encabezado en el componente Services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -6,8 +6,9 @@ const Services = () => {
       <div className="max-w-5xl mx-auto px-10 lg:px-20 py-20">
         <div className="flex flex-col lg:flex-row items-center justify-center gap-12">
           <div className="w-full text-center">
-            <h2 className="text-3xl md:text-4xl mb-8 text-white">Nuestros Servicios</h2>
-
+            <h2 className="text-3xl md:text-4xl mb-8 text-white">
+              Nuestros Servicios
+            </h2>
             <div className="space-y-6">
               <p className="text-white">
                 En WebNova no solo desarrollamos soluciones; las creamos con precisión y pasión.
@@ -18,6 +19,14 @@ const Services = () => {
                 para impulsar tu negocio al futuro. ¡Descubre cómo podemos revolucionar tu presencia en línea!
               </p>
             </div>
+          </div>
+
+          <div className="w-full flex items-center justify-center">
+            <img
+              src="/ruta/de/tu-imagen.jpg"
+              alt="Descripción de la imagen"
+              className="max-w-full h-auto"
+            />
           </div>
         </div>
       </div>
